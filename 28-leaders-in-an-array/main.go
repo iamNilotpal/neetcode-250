03e58a7be5c5f97e10604e61cd8252fda8f805f4
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2025-06-05 12:09:58 +0530

    leaders in an array optimal solution

--- 28-leaders-in-an-array/main.go
+++ 28-leaders-in-an-array/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func leadersInAnArrayBrute(nums []int) []int {
 	result := make([]int, 0)
@@ -22,7 +25,24 @@ func leadersInAnArrayBrute(nums []int) []int {
 	return result
 }
 
+func leadersInAnArrayOptimal(nums []int) []int {
+	max := math.MinInt
+	result := make([]int, 0)
+
+	for i := len(nums) - 1; i >= 0; i-- {
+		if nums[i] > max {
+			max = nums[i]
+			result = append(result, nums[i])
+		}
+	}
+
+	return result
+}
+
 func main() {
 	nums := []int{9, 6, 4, 2, 3, 5, 7, 0, 1}
 	fmt.Printf("leadersInAnArrayBrute: %+v \n", leadersInAnArrayBrute(nums))
+
+	nums = []int{10, 22, 12, 3, 0, 6}
+	fmt.Printf("leadersInAnArrayOptimal: %+v \n", leadersInAnArrayOptimal(nums))
 }
