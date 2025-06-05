a7def43fbdb5d5a727d32d4d5aa28dc60e7969ea
Nilotpal Deka <iamnilotpaldeka@gmail.com>
2025-06-05 14:33:56 +0530

    set matrix zeros better solution

--- 30-set-matrix-zeros/main.go
+++ 30-set-matrix-zeros/main.go
@@ -2,6 +2,24 @@ package main
 
 import "fmt"
 
+func markRow(matrix [][]int, row int) {
+	cols := len(matrix[0])
+	for col := range cols {
+		if matrix[row][col] != 0 {
+			matrix[row][col] = -1
+		}
+	}
+}
+
+func markColumn(matrix [][]int, col int) {
+	rows := len(matrix)
+	for row := range rows {
+		if matrix[row][col] != 0 {
+			matrix[row][col] = -1
+		}
+	}
+}
+
 func setZeroesBruteForce(matrix [][]int) {
 	rows := len(matrix)
 	cols := len(matrix[0])
@@ -24,27 +42,37 @@ func setZeroesBruteForce(matrix [][]int) {
 	}
 }
 
-func markRow(matrix [][]int, row int) {
+func setZeroesBetter(matrix [][]int) {
+	rows := len(matrix)
 	cols := len(matrix[0])
-	for col := range cols {
-		if matrix[row][col] != 0 {
-			matrix[row][col] = -1
+
+	tempRows := make([]int, rows)
+	tempCols := make([]int, cols)
+
+	for row := range rows {
+		for col := range cols {
+			if matrix[row][col] == 0 {
+				tempRows[row] = 1
+				tempCols[col] = 1
+			}
 		}
 	}
-}
 
-func markColumn(matrix [][]int, col int) {
-	rows := len(matrix)
 	for row := range rows {
-		if matrix[row][col] != 0 {
-			matrix[row][col] = -1
+		for col := range cols {
+			if tempRows[row] == 1 || tempCols[col] == 1 {
+				matrix[row][col] = 0
+			}
 		}
 	}
 }
 
 func main() {
 	matrix := [][]int{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}}
-
 	setZeroesBruteForce(matrix)
 	fmt.Printf("setZeroesBruteForce : %+v \n", matrix)
+
+	matrix = [][]int{{0, 1, 2, 0}, {3, 4, 5, 2}, {1, 3, 1, 5}}
+	setZeroesBetter(matrix)
+	fmt.Printf("setZeroesBetter : %+v \n", matrix)
 }
