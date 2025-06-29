793eceb494fa7bd1982e5a820c40816e6a1cec2e
iamBelugaa <iamnilotpaldeka@gmail.com>
2025-06-29 11:58:35 +0530

    find an element that appears once better + optimal solution

--- 25-find-element-that-appear-once/main.go
+++ 25-find-element-that-appear-once/main.go
@@ -24,7 +24,31 @@ func findElementBruteForce(nums []int) int {
 	return math.MinInt
 }
 
+func findElementBetter(nums []int) int {
+	frequency := make(map[int]int, len(nums))
+	for _, v := range nums {
+		frequency[v] += 1
+	}
+
+	for key, val := range frequency {
+		if val == 1 {
+			return key
+		}
+	}
+	return math.MinInt
+}
+
+func findElementOptimal(nums []int) int {
+	var xor int
+	for _, v := range nums {
+		xor ^= v
+	}
+	return xor
+}
+
 func main() {
 	arr := []int{1, 1, 1, 1, 0, 2, 2, 3, 3}
 	fmt.Printf("findElementBruteForce: %+v \n", findElementBruteForce(arr))
+	fmt.Printf("findElementBetter: %+v \n", findElementBetter(arr))
+	fmt.Printf("findElementOptimal: %+v \n", findElementOptimal(arr))
 }
